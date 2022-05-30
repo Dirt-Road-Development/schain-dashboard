4cec482e2919adb9bd98291871d064cec74ce8a9
Sawyer Cutler <sawyer.cutler@gmail.com>
2022-05-30 10:33:41 -0700

    Npm Core Libraries and Config and State Added

--- src/ui/dashboard/badges.js
+++ src/ui/dashboard/badges.js
@@ -5,7 +5,7 @@ const BadgesContainer = styled.div`
     position: absolute;
     bottom: 5%;
     left: 2.5%;
-    height: 30%;
+    height: 32%;
     width: 60%;
     border: 2px solid yellow;
     border-radius: 0 0 0 16px;
