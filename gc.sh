1231623d573beac6418be8914dd28b5f6e0a74e8
Sawyer Cutler <sawyer.cutler@gmail.com>
2022-05-30 07:08:58 -0700

    Install NPM Packages

---
+++ gc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+commitMsg=$@
+branch="$(git rev-parse --abbrev-ref HEAD)"
+echo $commitMsg
+echo $branch
+
+git add .
+
+git commit -am "$HEAD $commitMsg"
+
+git push -u origin "$branch"
+
+gh pr create
+
+gh pr merge
+
+git checkout development
+
+git pull --no-ff
+
+vim :qa!
