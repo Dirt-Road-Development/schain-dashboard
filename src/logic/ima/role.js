c95546d929bfd7cff0581cbe5f06a0aa0b0e5ee2
Sawyer Cutler <sawyer.cutler@gmail.com>
2022-06-11 15:23:33 -0700

    Assign Minter Role to TokenManager ERC Complete

--- src/logic/ima/role.js
+++ src/logic/ima/role.js
@@ -1,4 +1,5 @@
 import { ethers } from "ethers";
+import addresses from "../../config/addresses";
 
 class IMARole {
 
@@ -7,6 +8,85 @@ class IMARole {
         this.contract = new ethers.Contract(address, abi, this.provider.getSigner());
     }
 
+    async assignRole(isS2S, role, type) {
+        try {
+            const _assignType = this._getFunctionType(role);
+            const _tokenManagerAddress = this._getTokenManagerAddress(type);
+            if (_assignType === 'addMinter') {
+                /// Add Token Manager via addMinter Function
+                return await this._addMinter(_tokenManagerAddress);
+            } else if (_assignType === 'grantRole') {
+                /// Add Token Manager via grantRole Function
+                return await this._grantRole(_tokenManagerAddress);
+            } else {
+                /// Throw Error (Should be Impossible)
+                throw new Error('Impossible');
+            }
+        } catch (err) {
+            console.log(err);
+            throw new Error(err);
+        }
+    }
+
+    /// Checks for How to Assign Minter Role to Contract
+    _getFunctionType(type) {
+        // const possibleRoles = ['MINTER_ROLE'];
+        // const possibleFunctions = ['grantRole', 'addMinter'];
+        console.log(this.contract);
+        if (this.contract['addMinter']) {
+            return 'addMinter';
+        } else if (this.contract['grantRole']) {
+            if (this.contract['MINTER_ROLE']) {
+                return 'grantRole';
+            } else {
+                throw new Error(`This contract does not contain ${type} functionality`);
+            }
+        } else {
+            throw new Error(`This contract does not contain ${type} functionality`);
+        }
+        
+    }
+
+    /// Gets Address of Token Manager
+    _getTokenManagerAddress(type) {
+        return addresses[`token_manager_${type}`];
+    }
+
+    /// Assigns Minter role via addMinter Function
+    async _addMinter(assigneeAddress) {
+        try {
+            console.log("ADD MINTER");
+        } catch (err) {
+            throw new Error(err);
+        }    
+    }
+
+    /// Assigns Minter Role via addMinter Function
+    async _grantRole(assigneeAddress) {
+        console.log(this.contract);
+        try {
+            const MINTER_ROLE = await this.contract.callStatic.MINTER_ROLE();
+            const _hasRoleBefore = await this.contract.callStatic.hasRole(MINTER_ROLE, assigneeAddress);
+            if (_hasRoleBefore) {
+                return {
+                    hasRole: _hasRoleBefore
+                };
+            }
+            const _assignFn = await (await this.contract.grantRole(MINTER_ROLE, assigneeAddress, {
+                gasLimit: ethers.utils.hexlify(5000000)
+            })).wait();
+
+            const _hasRoleAfter = await this.contract.callStatic.hasRole(MINTER_ROLE, assigneeAddress);
+
+            return {
+                txHash: _assignFn,
+                hasRole: _hasRoleAfter
+            };
+        } catch (err) {
+            console.log("ERROR: ", err);
+            throw new Error('Error Granting Role');
+        }
+    }
 }
 
 export {
---
+++ src/ui/ima/actions/roles/assign.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import styled from "styled-components";
+import { MinterBurnerButton } from "./button";
+const AssignContainer = styled.div`
+    margin: 16px 0;
+    padding: 16px 0;
+    height: 100%;
+    width: 100%;
+`;
+
+const Assign = ({ state, assignRole, isS2S }) => {
+
+    return (
+        <AssignContainer>
+            <MinterBurnerButton type="Minter" assignRole={assignRole} />
+            {isS2S && <MinterBurnerButton type="Burner" assignRole={assignRole} />}
+        </AssignContainer>
+    );
+}
+
+
+export {
+    Assign
+}
